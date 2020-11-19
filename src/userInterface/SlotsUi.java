ef231c0f643f27a3a39eff57f11ca82fdf7c4b04
Adam Fernandes <fernadam8@gmail.com>
2020-11-18 23:31:36 -0500

    syncing everything currently

---
+++ src/userInterface/SlotsUi.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package userInterface;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author quikks1lver
+ */
+public class SlotsUi extends JPanel
+{
+    
+}
